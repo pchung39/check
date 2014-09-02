ef3e83bb0af7a682a55203d9bafa7ec137d4ab8d
Paul Chung <paulchung39@gmail.com>
2014-09-01 23:39:13 -0700

    recommit ip_sign_in columns to "user" table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140901180518) do
+ActiveRecord::Schema.define(version: 20140902044307) do
 
   create_table "add_admin_to_roles", force: true do |t|
     t.string   "admin"
@@ -71,6 +71,8 @@ ActiveRecord::Schema.define(version: 20140901180518) do
     t.datetime "updated_at"
     t.string   "name"
     t.integer  "roles_mask"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
